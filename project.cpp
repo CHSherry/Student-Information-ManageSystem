2e6ddcb879ecb5a036d0c55afc4801b79bc192a9
missing-ghost <65663325+missing-ghost@users.noreply.github.com>
2020-08-10 18:46:26 +0800

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,4 +1,30 @@
 #include<stdio.h>   /*Student Information ManageSystem*/
+#include<stdlib.h>
+
+void close(void)
+{
+    puts("                     感谢使用！");
+    puts("######################################################");
+    exit(0);  //退出程序
+}
+
+void help(void)  //待定
+{
+    puts("");
+}
+
+void about(void)  //待定
+{
+
+}
+
+void analy(void)  //未完成
+{
+    puts("请选择一项进行分析：");
+    puts("1.求平均值与最值");
+    puts("2.排序");
+    puts("3.正态分布分析");
+}
 int main(void)
 {
 
